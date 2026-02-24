4641b471aa352f467f2007ceebad340b76d32ad1
AriasLcr <ariaslcr@gmail.com>
2026-02-23 19:03:23 -0500

    GitHub Social Sign on set up

---
+++ backend/src/main/java/com/makerspace/backend/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.makerspace.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Home!";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, secured!";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "An error occurred!";
+    }
+}
---
+++ backend/src/test/java/com/makerspace/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makerspace.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
